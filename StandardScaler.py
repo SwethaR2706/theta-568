04034b5830ac62a347806cd1aa8c173bcaabd380
SwethaR2706 <swetharavuri27@gmail.com>
2025-01-27 07:14:43 +0000

    50

---
+++ StandardScaler.py
@@ -0,0 +1,18 @@
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+
+data = {
+    'Age': [25, 30, 35, 40, 45],
+    'Height': [150, 160, 170, 180, 190],  
+    'Weight': [50, 60, 70, 80, 90]
+}
+
+df = pd.DataFrame(data)
+print("Original DataFrame:")
+print(df)
+scaler = StandardScaler()
+
+normalized_data = scaler.fit_transform(df)
+normalized_df = pd.DataFrame(normalized_data, columns=df.columns)
+print("\nNormalized DataFrame (scaled to range [0,1]):")
+print(normalized_df)
