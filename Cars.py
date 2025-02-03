d718a25dfa3095932bcac6731dfe7581a2646cb6
SwethaR2706 <swetharavuri27@gmail.com>
2025-02-03 15:35:54 +0000

    0

---
+++ Cars.py
@@ -0,0 +1,8 @@
+import pandas as pd
+from google.colab import files
+uploaded=files.upload()
+file_name=list(uploaded.keys())[0]
+df=pd.read_csv(file_name)
+print(df.head)
+df
+df.corr(numeric_only=True)
