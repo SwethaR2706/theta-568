f90ebc942045230413983f6a590154916f584569
SwethaR2706 <swetharavuri27@gmail.com>
2025-02-05 15:33:24 +0000

    d

---
+++ Cars_new.py
@@ -0,0 +1,9 @@
+import pandas as pd
+from google.colab import files
+uploaded=files.upload()
+file_name=list(uploaded.keys())[0]
+cars=pd.read_csv(file_name)
+car1 = cars.drop(cars.index[[70, 76]], axis=0).reset_index()
+car1=car1.drop(['index'],axis=1)
+car1
+final_ml_v=smf.ols('MPG~VOL+SP+HP',data=car1).fit()
